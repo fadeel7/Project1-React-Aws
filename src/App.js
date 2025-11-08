7c6cd78bf2fb2c4ecab3ea271d876d77368912aa
fadeel_d <104166805+fadeel7@users.noreply.github.com>
2025-11-08 16:12:07 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Welcome to My React Website</h1>
+      <p>This site is hosted on AWS S3!</p>
+    </div>
+  );
+}
+
+export default App;
